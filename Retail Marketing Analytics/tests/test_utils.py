1e8dc6f2b7e58c5f2583f5f711b5e830ba6698dd
AVANTHI NAMALA <avanthinamala8317@gmail.com>
2025-08-24 18:13:37 -0400

    Update test_utils.py

--- Retail Marketing Analytics/tests/test_utils.py
+++ Retail Marketing Analytics/tests/test_utils.py
@@ -1,1 +1,20 @@
+```python
+from src.common import utils
 
+def test_has_meta_columns():
+    assert isinstance(utils.BRONZE_META_COLS, list)
+```
+
+### `tests/test_scd2.py`
+
+```python
+import pyspark
+from pyspark.sql import SparkSession
+from src.common.scd2 import scd2_upsert
+
+# Note: In CI, you can skip heavy Spark tests or use local[1]
+
+def test_scd2_signature():
+    # Basic import test
+    assert callable(scd2_upsert)
+```
